e08b9abf25fb57c7a0ad19852e07b1ec2bab38ee
kovvuruRajesh <111898885+kovvuruRajesh@users.noreply.github.com>
2024-06-04 11:24:23 +0530

    Add files via upload

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
+import NewCollections from '../Components/NewCollections/NewCollections';
+import Newsletter from '../Components/NewsLetter/NewsLetter';
+
+
+const Shop = () => {
+
+    return(
+        <div >
+            <Hero/>
+            <Popular/>
+            <Offers/>
+            <NewCollections/>
+            <Newsletter/>
+        </div>
+
+    );
+}
+
+export default Shop
